5a91ea9e504f8b0869b27d89f936d292e044719f
mcshakes <efmccarthy26@gmail.com>
2020-10-03 10:07:10 -0600

    Fixed useEffect function

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddModal from "../components/AddModal";
 import { makeStyles } from '@material-ui/core/styles';
-
+import RestaurantFinder from "../apis/RestaurantFinder";
+import RestaurantList from "../components/RestaurantList"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,11 +15,25 @@ const useStyles = makeStyles((theme) => ({
 const Home = () => {
     const classes = useStyles();
 
-    // const [addFormStatus, changeAddForm] = useState(false);
+    useEffect(() => {
 
-    // const handleRestaurantForm = () => {
-    //     changeAddForm(true);
-    // }
+
+
+        try {
+            const fetchData = async () => {
+                const response = await RestaurantFinder.get("/")
+                console.log(response)
+            }
+
+            fetchData()
+
+        } catch (err) {
+            console.log("Error Fetching All Restaurants\n", err)
+        }
+
+
+    }, [])
+
 
     return (
         <div>
@@ -27,7 +42,8 @@ const Home = () => {
             </div>
             <AddModal />
             <div className="list">
-                WHERE LIST WILL bE!
+                <RestaurantList />
+
             </div>
 
         </div>
