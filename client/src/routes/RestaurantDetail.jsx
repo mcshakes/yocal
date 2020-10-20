5e2b24a0a7a7bd9b837d9ed1f8b4bf50d599c41a
mcshakes <efmccarthy26@gmail.com>
2020-10-20 16:55:50 -0600

    Passing context of selectedRestaurant around

--- client/src/routes/RestaurantDetail.jsx
+++ client/src/routes/RestaurantDetail.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 const RestaurantDetail = () => {
+    const { id } = useParams();
+    const { selectedRestaurant, setSelectedRestaurant } = useContext(RestaurantsContext)
+
     return (
         <div>
-            <h1>DETAILS</h1>
+            <h1>{id}</h1>
         </div>
     )
 }
