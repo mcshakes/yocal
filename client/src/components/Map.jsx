301d2a00769379f75ee69eaf12939d00d616d3e3
mcshakes <efmccarthy26@gmail.com>
2020-10-21 06:58:16 -0600

    Moved things around and pushing up before making changes to DB and mapbox

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 
-const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+const MAPBOX_TOKEN = "";
 
 function Map() {
     const [viewport, setViewport] = useState({
@@ -12,7 +12,7 @@ function Map() {
         bearing: 0,
         pitch: 0
     });
-    console.log("FUKC", MAPBOX_TOKEN)
+
     return (
         <ReactMapGL
             {...viewport}
