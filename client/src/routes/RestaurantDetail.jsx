fdaf54f0c3c9594346e8b6790a6dbc22d707ee1d
mcshakes <efmccarthy26@gmail.com>
2020-10-23 17:47:07 -0600

    Problem with query

--- client/src/routes/RestaurantDetail.jsx
+++ client/src/routes/RestaurantDetail.jsx
@@ -2,11 +2,51 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
-import Map from "../components/Map";
+import { useQuery, gql } from '@apollo/client';
+
+const RESTAURANT_DATA = gql`
+{   
+    query getRestaurantData($term: String, $location: String, $limit: Int) {
+        search(
+            term:"shake shack",
+            location:"denver",
+            limit: 1
+          ) {
+            business {
+              coordinates {
+                latitude
+                longitude
+              }
+              categories {
+                title
+                alias
+              }
+              attributes {
+                wheelchair_accessible {
+                  name_code
+                  value_code
+                }
+              }
+              rating
+              price
+              review_count
+              reviews {
+                text
+                rating
+                time_created
+              }
+            }
+          }
+    }
+    
+  }
+`;
 
 const RestaurantDetail = () => {
     const { id } = useParams();
     const { selectedRestaurant, setSelectedRestaurant } = useContext(RestaurantsContext)
+    const { loading, error, data } = useQuery(RESTAURANT_DATA);
+
 
     useEffect(() => {
         try {
