ae55e11f56415cac15bf3c31f43bc0ec36e19123
mcshakes <efmccarthy26@gmail.com>
2021-01-25 20:48:35 -0700

    Some testing for shape of response

--- server/tests/restaurants.test.js
+++ server/tests/restaurants.test.js
@@ -6,25 +6,40 @@ const api = supertest(app)
 
 describe("Restaurant Service", () => {
 
-    // let server;
-
     beforeAll(async() => {
         await createTable('test_restaurants')
-        await insertRestaurant('Pasta Joint', "3800 Perry St", "Denver", "80212", 3, "Italian, Noodles")
-        await insertRestaurant('Naan & Curry place', "1200 Testing St", "Fort Collins", "80212", 3, "Indian")
+        await insertRestaurant('test_restaurants', 'Pasta Joint', "3800 Perry St", "Denver", "80212", 3, "Italian, Noodles")
+        await insertRestaurant('test_restaurants', 'Naan & Curry place', "1200 Testing St", "Fort Collins", "80212", 3, "Indian")
 
         // const mod = await import("../server")
         // server = mod
     })
 
     describe("fetch all restaurants", () => {
-        // Problem with this is we are always fetching a specific table in GET, so we need to bring in db-utils to our endpoints;
         
+        test("response has proper data properties", async () => {
+            const response = await api.get("/api/v1/restaurants")
+
+            expect(response.body).toHaveProperty("status")
+            expect(response.body).toHaveProperty("results")
+            expect(response.body).toHaveProperty("data")
+            
+            expect(response.body.status).toBeString()
+            expect(response.body.results).toBeNumber();
+            expect(response.body.data.restaurants).toBeArray()
+        })
+
         test("restaurants are returned as JSON", async () => {
             await api
                 .get("/api/v1/restaurants")
                 .expect(200)
                 .expect('Content-Type', /application\/json/)
+        })
+
+        test("all restaurants are returned", async () => {
+            const response = await api.get("/api/v1/restaurants")
+            // expect(response.body).toBeInstance
+            expect(response.body.data).not.toBeEmpty()
         })
     })
 
